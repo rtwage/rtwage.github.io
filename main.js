0978c502904b65819dd6c37a11ba1cb7904dd629
Vincent Philaeux Lamotte <vinc.lamotte@gmail.com>
2016-11-18 13:52:35 +0100

    Basic counter

---
+++ main.js
@@ -0,0 +1,31 @@
+var app = angular.module('app', []);
+
+app.controller('Controller', function Controller($scope) {
+
+    $scope.counter = 0;
+    $scope.display_counter = Math.round($scope.counter);
+
+    $scope.increment = 4000;
+    $scope.monthly = true; // otherwise it is annuary
+
+    $scope.refresh_rate = 1;
+
+    function incr() {
+
+        var amount = 0; // Value won per second
+        amount = $scope.increment / 30.0 / 24.0 / 60.0 / 60.0; //
+        if (!$scope.monthly) {
+            amount = amount / 365;
+        }
+
+        amount = amount * $scope.refresh_rate / 1000.0; // apply refresh rate
+
+        $scope.counter = $scope.counter + amount
+        $scope.display_counter = Math.round($scope.counter);
+
+        $scope.$apply();
+    }
+
+    $scope.interval = setInterval(incr, $scope.refresh);
+
+});
