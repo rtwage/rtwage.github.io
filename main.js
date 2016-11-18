882eb8a0ab37804a67464a46e3e4e628fe6985ae
Vincent Philaeux Lamotte <vinc.lamotte@gmail.com>
2016-11-18 16:34:22 +0100

    Add point of start

--- main.js
+++ main.js
@@ -7,25 +7,38 @@ app.controller('Controller', function Controller($scope) {
 
     $scope.salary = 4000;
     $scope.salaryFrequency = "monthly";
+    $scope.startAt = "now"
 
-    $scope.refresh_rate = 1;
+    var start_tick = 0
+    var refresh_rate = 10;
+    var page_open = new Date()
 
-    function incr() {
-
-        var amount = 0; // Value won per second
-        amount = $scope.salary / 30.0 / 24.0 / 60.0 / 60.0; //
-        if ($scope.salaryFrequency == "yearly") {
-            amount = amount / 365;
+    function update() {
+        // Update Start
+        if ($scope.startAt === "now") {
+            start_tick = page_open.getTime()
+        } else if ($scope.startAt === "month") {
+            start_tick = new Date(page_open.getFullYear(), page_open.getMonth(), 0, 0, 0, 0, 0).getTime()
+        } else if ($scope.startAt === "year") {
+            start_tick = new Date(page_open.getFullYear(), 0, 0, 0, 0, 0, 0).getTime()
         }
 
-        amount = amount * $scope.refresh_rate / 1000.0; // apply refresh rate
+        // Update Diff
+        var now = new Date().getTime()
+        var diff = (now - start_tick) / 1000.0
 
-        $scope.counter = $scope.counter + amount;
-        $scope.display_counter = Math.round($scope.counter);
+        var second_gain = $scope.salary / 30.0 / 24.0 / 60.0 / 60.0;
 
+        if ($scope.salaryFrequency == "yearly") {
+            second_gain = second_gain  / 365;
+        }
+
+        // Display
+        $scope.counter = diff * second_gain
+        $scope.display_counter = $scope.counter.toFixed(5);
         $scope.$apply();
     }
 
-    $scope.interval = setInterval(incr, $scope.refresh_rate);
+    $scope.interval = setInterval(update, refresh_rate);
 
 });
